cc221f72ea86f203f0d0630ee3f8148ecc3cc179
Alex <hills.alex@gmail.com>
2011-03-20 21:55:54 -0400

    Actually changed the files this time...

--- search1/createIndex.py
+++ search1/createIndex.py
@@ -1,80 +1,115 @@
-'''
-Created on Feb 15, 2011
-
-@author: Alex
-'''
-
-#input: stop words, pages, index to build, title index to build
-
-#get pageID, title, text by parsing page
-#store pageID and title in title doc..
-#for title and text: concat total, with a newline. get stream of words.
-#lower case it all
-#get all the tokens
-#filter out tokens matching stop words
-#stem tokens with porter stemmer
-
 import sys
 import re
 import PorterStemmer
+import math
 import time
+#setup: collection words:
+#(document frequency, collection docs)
+#docs: (docnumber, (loc1,loc2,loc3))
 
-words = {}
+#new format: word docfreq docnumber loc1 loc2 loc3 loc4, docnumber loc1 loc2 loc3
+
+words={}
 p = PorterStemmer.PorterStemmer()
 
-swf = open(sys.argv[1],'r')
-stopWords = swf.read()
-stopWords = stopWords.split('\n')
-swf.close()
-swf = open(sys.argv[2],'r')
-collectionString = swf.read()
-swf.close()
+#collectionString = swf.read()
+#swf.close()
 
 indexFile = open(sys.argv[3],'w')
-titleFile = open(sys.argv[4],'w')
+#titleFile = open(sys.argv[4],'w')
 
 idCheck = re.compile('(.*?)(<id>)(.*?)(</id>)', re.DOTALL)
 titleCheck = re.compile('(.*?)(<title>)(.*?)(</title>)', re.DOTALL)
 textCheck = re.compile('(.*?)(<text>)(.*?)(</text>)', re.DOTALL)
-pageSplit = re.compile('(</page>)(.*?)(<page>)', re.DOTALL)
 
-stopWordString = ''
-for i in range(0,len(stopWords)-1):
-    stopWordString += r'\b' + stopWords[i] + r'\b' + '|'
-stopWordString = stopWordString[:-1]
+'''HERE"S MY TEST'''
+words = {}
+
 
 def parsePage(page):
-    ID = idCheck.search(page).group(3)
-    title = titleCheck.search(page).group(3)
-    titleFile.write(str((ID,title)) + '\n')
-    text = ' ' + textCheck.search(page).group(3)
-    text += " " + title;
-    text = text.lower()
-    text = re.sub('(_|\n)',' ',text)
-    text = re.sub(r'\W+',' ',text)
-    text = re.sub(stopWordString,' ',text)
-    text = re.sub('\s+',' ',text)
-    text = text.split(' ')
+	ID = idCheck.search(page).group(3)
+	title = titleCheck.search(page).group(3)
+	#titleFile.write(str((ID,title)) + '\n')
+	text = ' ' + textCheck.search(page).group(3)
+	text += " " + title;
+	text = text.lower()
+	text = re.sub('(_|\n)',' ',text)
+	text = re.sub(r'\W+',' ',text)
+	text = re.sub('\s+',' ',text)
+	text = text.split(' ')
+	for i in range(0,len(text)):
+		text[i] = p.stem(text[i], 0, len(text[i])-1)
+	text = [t for t in text if t not in stopWords]
+	for i in range(0,len(text)):
+		if text[i] in words:
+			wordresult = words[text[i]]
+			freq = wordresult[0]+1
+			if ID in wordresult[1]:
+				doclocations = wordresult[1][ID]
+				doclocations += ' ' + str(i)
+				wordresult[1][ID] = doclocations
+				wordresult=(freq,wordresult[1])
+			else:
+				doclocations = str(i)
+				wordresult[1][ID] = doclocations
+				wordresult=(freq,wordresult[1])
+			words[text[i]] = wordresult
+		else:
+			wordresult = (1,{ID:str(i)})
+			words[text[i]] = wordresult
+			
+
+
+
+def makeStopWords():
+	swf = open(sys.argv[1],'r')
+	stopWords = swf.read()
+	stopWords = stopWords.replace('\r','')
+	stopWords = stopWords.split('\n')
+	swf.close()
+	return stopWords
+
+
+def readFiles():
+	swf = open(sys.argv[2],'r')
+	swf.readline()
+	currentPage = ''
+	totalpages = 1
+	for line in swf:
+		if (not (line.find('<page>')>-1 or line.find('</page>')>-1)):
+			currentPage += line
+		if (line.find('</page>')>-1):
+			parsePage(currentPage)
+			totalpages=totalpages+1
+			currentPage=''
+	swf.close()
 	
-    for i in range(1,len(text)):
-        text[i] = p.stem(text[i], 0, len(text[i])-1)
-        idpair = (ID,i)
-        if text[i] in words:
-            words[text[i]].append(idpair)
-        else:
-            words[text[i]] = [idpair]
-def parseCollection(col,words):
-    pages = re.split('</page>\s*?<page>', col)
-    map(parsePage,pages)
+	for k,v in words.iteritems():
+		freq = v[0]
+		freq = math.log10(float(totalpages)/float(freq))
+		words[k] = (freq, v[1])
+
+def writeIndex(index):
+	for k,v in index.iteritems():
+		word = str(k)
+		freq = v[0]
+		indexFile.write(word + ' ' )
+		indexFile.write(str(freq) + ' ' )
+		docs = v[1]
+		for x,y in docs.iteritems():
+			indexFile.write(str(x) + ' ' + str(y) + ',')
+		indexFile.write('\n')
 
 start = time.time()
 
-parseCollection(collectionString,stopWords)
-for k,v in words.iteritems():
-        indexFile.write(str(k) + ' ' + str(v) + '\n')
+totalpages=0
+stopWords = makeStopWords()
+readFiles()
+writeIndex(words)
+	
 end = time.time()
- 
+
 elapsed= end - start
- 
+
 min = elapsed/60
-#print(min) #comment to not print runtime
+print('Time elapsed: ' + str(min)) #comment to not print runtime
