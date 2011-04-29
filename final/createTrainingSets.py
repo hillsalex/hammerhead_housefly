34923b85bc63ab234a3e7e9beec7efd14681680f
= <=>
2011-04-29 00:25:45 -0400

    more creating training

--- final/createTrainingSets.py
+++ final/createTrainingSets.py
@@ -1,3 +1,5 @@
+from random import random
+
 def vecToDic(V):
 	docDic = {}
 	lineNum = 0	
@@ -13,7 +15,8 @@ v = vecToDic('vecrep.dat')
 
 print "Done making vec dictionary."
 
-C = range(1)
+# Classes
+C = range(11)
 
 for c in C:
 	newFile = open('svmtraining'+str(c)+'.dat', 'w')
@@ -25,10 +28,27 @@ for c in C:
 		doc = int(docID)
 		if int(classID)==c:
 			newFile.write("+1 ")
-			for term in v[doc].keys():
+			for term in sorted(v[doc].keys()):
 				newFile.write(str(term)+":"+str(v[doc][term])+" ")
+			newFile.write("\n")
 		else:
-			newFile.write("-1 ")
-			for term in v[doc].keys():
-				newFile.write(str(term)+":"+str(v[doc][term])+" ")
+			if int(random()*100)==0: # Write 1/100 of the 10000 negative examples
+				newFile.write("-1 ")
+				for term in sorted(v[doc].keys()):
+					newFile.write(str(term)+":"+str(v[doc][term])+" ")
+				newFile.write("\n")
 		count += 1
+	# @ repeat N times...
+	"""f = open('training.dat','r')
+	for line in f:
+		docID, classID = line.rstrip('\n').split(' ')
+		doc = int(docID)
+		if not int(classID)==c:
+			if int(random()*10)==0: # 1/10th of negative examples
+				# write to some temp file
+				# run svm_learn on that temp file
+				# run svm_classify on the resulting model
+				# iterate through prediction to find lowest score
+				# write that doc and its features to svmtrainingX with -1"""
+				
+
